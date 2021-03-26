0f1c0dc460c3f70673f47851ef9c8adaa32f0f8c
Alfredo Sanchez <alfredosanchez080114@gmail.com>
2021-03-26 14:21:49 -0300

    mensaje de socio no exitente

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -3,21 +3,28 @@ const socioName = document.getElementById('name')
 const socioNumber = document.getElementById('socio-number')
 const socioStatus = document.getElementById('status')
 
+const limpiarCampos = ()=>{
+    socioName.innerHTML = `<b>Nombre:</b> Socio no encontrado`;
+    socioNumber.innerHTML = `<b>Socio N.°:</b>`;
+    socioStatus.innerHTML = `<b>Estado:</b>`;
+}
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault()
     // console.log(e.target.ci.value)
     let cedula = e.target.ci.value;
       let ci =  cedula.replace(/\s+|\-|\./g, '')
-    fetch(`http://192.168.1.19:3000/socio/${ci}`)
-    .then(res => res.ok ? res.json() : console.log(`error`))
+    fetch(`http://www.cofudep.coop.py:3000/socio/${ci}`)
+    .then(res => res.ok ? res.json() : limpiarCampos() )
     .then(data =>{
         const socio = data
         console.log(socio)
-        socioName.innerHTML = `<b>Nombre:</b> ${data[0].socio_nombre}`;
-        socioNumber.innerHTML = `<b>Socio N.°:</b> ${data[0].socio_nro}`;
-        socioStatus.innerHTML = `<b>Estado:</b> ${data[0].socio_estado}`;
+        socioName.innerHTML = `<b>Nombre:</b> ${socio[0].socio_nombre}`;
+        socioNumber.innerHTML = `<b>Socio N.°:</b> ${socio[0].socio_nro}`;
+        socioStatus.innerHTML = `<b>Estado:</b> ${socio[0].socio_estado}`;
     })
-    .catch(e => console.log(e))
+    .catch(e => {
+       return console.log(e)
+    })
     e.target.ci.value = ""
 })
